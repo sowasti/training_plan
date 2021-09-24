50e0048fbab996c4fbcf13c0d42f88e85c150347
Anastasiya <sowa.anast@ya.ru>
2021-09-24 20:45:45 +0300

    add vk UI and icons and bridge and file vk-hosting-config.json

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const MainPage: React.FC = ()=> {
+const MainPage: React.FC = () => {
   return (
-    <div>
-      gfhfghfghfghfg
-    </div>
+   <div>
+     
+   </div>
+
   );
 }
 
