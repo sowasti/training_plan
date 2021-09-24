4beb4323842b6d69a149ad9be16999ea80940767
Anastasiya <sowa.anast@ya.ru>
2021-09-24 18:42:10 +0300

    add structure components in app

---
+++ src/components/AddExercisePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddExercisePage: React.FC = ()=> {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default AddExercisePage;
---
+++ src/components/TrainItemPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrainItemPage: React.FC = ()=> {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default TrainItemPage;
