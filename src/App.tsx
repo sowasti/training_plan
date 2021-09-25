43a278787d78815af68ec713afd6f1c2d787e511
Anastasiya <sowa.anast@ya.ru>
2021-09-25 17:02:41 +0300

    stylization exercise page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AdaptivityProvider, AppRoot, ConfigProvider, View } from "@vkontakte/vkui";
+import { AdaptivityProvider, AppRoot, ConfigProvider, SplitCol, SplitLayout, View } from "@vkontakte/vkui";
 import '@vkontakte/vkui/dist/vkui.css';
 import MainPage from "./components/MainPage";
 import AddProgramPage from "./components/AddProgramPage";
@@ -12,15 +12,19 @@ const App: React.FC = () => {
   return (
     <ConfigProvider>
       <AdaptivityProvider>
-        <AppRoot>          
-              <View activePanel="addProgram">
+        <AppRoot>
+          <SplitLayout>
+            <SplitCol>
+              <View activePanel="exercise">
                 <MainPage id="main" />
                 <AddProgramPage id="addProgram" />
                 <MuscleGroupPage id="muscleGroup" />
                 <AddExercisePage id="addExercise" />
                 <TrainItemPage id="train" />
                 <ExercisePage id="exercise" />
-              </View>          
+              </View>
+            </SplitCol>
+          </SplitLayout>
         </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
--- src/components/ExercisePage.tsx
+++ src/components/ExercisePage.tsx
@@ -1,14 +1,50 @@
-import { Panel } from "@vkontakte/vkui";
 import React from "react";
-
-interface IExerciseProps{
+import { Button, Div, FixedLayout, FormItem, FormLayout, Group, Header, IconButton, Input, Panel, PanelHeader, PanelHeaderBack } from "@vkontakte/vkui";
+import { Icon36Done } from '@vkontakte/icons';
+interface IExerciseProps {
   id: string
 }
 
-const ExercisePage: React.FC<IExerciseProps> = ({id})=> {
+const ExercisePage: React.FC<IExerciseProps> = ({ id }) => {
   return (
     <Panel id={id}>
-     
+      <PanelHeader left={<PanelHeaderBack />}>Скручивания лежа</PanelHeader>
+      <Group header={<Header mode="secondary">Заполните данные по упражнению</Header>}>
+        <FormLayout>
+          <FormItem top="Количество подходов">
+            <Input type="number" />
+          </FormItem>
+          <FormItem top="1 подход">
+            <div style={{ display: "flex", justifyContent: 'space-between' }}>
+              <p>Вес</p>
+              <Input type="number" />
+              <p>Повторений</p>
+              <Input type="number" />
+            </div>
+          </FormItem>
+          <FormItem top="2 подход">
+            <div style={{ display: "flex", justifyContent: 'space-between' }}>
+              <p>Вес</p>
+              <Input type="number" />
+              <p>Повторений</p>
+              <Input type="number" />
+            </div>
+          </FormItem>
+          <FormItem top="3 подход">
+            <div style={{ display: "flex", justifyContent: 'space-between' }}>
+              <p>Вес</p>
+              <Input type="number" />
+              <p>Повторений</p>
+              <Input type="number" />
+            </div>
+          </FormItem>
+        </FormLayout>
+        <FixedLayout>
+          <Div>
+            <Button><Icon36Done /></Button>
+          </Div>
+        </FixedLayout>
+      </Group>
     </Panel>
   );
 }
