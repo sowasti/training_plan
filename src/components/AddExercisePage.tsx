edee3de89fe3bd530a8430b77692cdb8099eb0ef
Anastasiya <sowa.anast@ya.ru>
2021-09-30 15:21:10 +0300

    change stylization addExercisePage

--- src/components/AddExercisePage.tsx
+++ src/components/AddExercisePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Div, FixedLayout, Group, IconButton, List, Panel, PanelHeader, PanelHeaderBack, SimpleCell } from "@vkontakte/vkui";
-import { Icon24AddOutline } from '@vkontakte/icons';
+import { Avatar, Cell, Group, Header, List, Panel, PanelHeader, PanelHeaderBack, } from "@vkontakte/vkui";
+import { Icon20ChevronRightOutline } from '@vkontakte/icons';
 import { withRouter } from 'react-router-vkminiapps';
 import { useTypeSelector } from "../hooks/useTypeSelector";
 interface IAddExerciseProps {
@@ -9,23 +9,24 @@ interface IAddExerciseProps {
 }
 
 const AddExercisePage: React.FC<IAddExerciseProps> = ({ id, router }) => {
-  const {activeExercise} = useTypeSelector(state=> state.app); 
-  console.log(activeExercise)
+  const { activeExercise, activeMuscleGroup } = useTypeSelector(state => state.app);
+
   return (
     <Panel id={id}>
-      <PanelHeader left={<PanelHeaderBack onClick={()=> router.toBack()}/>}>fhfggdhgfh</PanelHeader>
-      <Group style={{ paddingBottom: 60 }}>
-      <List>
-        {activeExercise.map((item, i)=>
-           <SimpleCell key={i} disabled after={<IconButton>< Icon24AddOutline /></IconButton>}>{item.name}</SimpleCell>
-          )}          
+      <PanelHeader left={<PanelHeaderBack onClick={() => router.toBack()} />}>{activeMuscleGroup}</PanelHeader>
+      <Group style={{ paddingBottom: 60 }} header={<Header mode="secondary">Список упражнений</Header>}>
+        <List>
+          {activeExercise.map((item, i) =>
+            <Cell
+              key={i}
+              before={<Avatar style={{ background: "#3f8ae0" }} size={10} />}
+              after={<Icon20ChevronRightOutline />}
+            >
+              {item.name}
+            </Cell>
+          )}
         </List>
       </Group>
-      <FixedLayout vertical="bottom">
-        <Div>
-          <Button stretched mode='tertiary' size="m" onClick={()=> router.toPanel("addProgramPage")}>Вернуться к добавлению программы</Button>
-        </Div>
-      </FixedLayout>
     </Panel>
   );
 }
