226082c4c659534bd36e8b1e2b303c559ebf0068
Anastasiya <sowa.anast@ya.ru>
2021-09-30 15:06:38 +0300

    save name activeMuscleGroup in store on MuscleGroupPage

--- src/components/MuscleGroupPage.tsx
+++ src/components/MuscleGroupPage.tsx
@@ -11,32 +11,40 @@ interface IMuscleGroupProps {
 
 const MuscleGroupPage: React.FC<IMuscleGroupProps> = ({ id, router }) => {
   const { muscleGroup } = useTypeSelector(state => state.app);
-  const { setActiveExercise } = useActions();
+  const { setActiveExercise, setActiveMuscleGroup } = useActions();
 
   const openMuscleGroup = (name: string) => {    
     switch (name) {
       case "Плечи":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[0].exercise);
         break;
       case "Руки":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[1].exercise);
         break;
       case "Спина":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[2].exercise);
         break;
       case "Поясница":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[3].exercise);
         break;
       case "Грудь":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[4].exercise);
         break;
       case "Пресс":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[5].exercise);
         break;
       case "Ягодицы":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[6].exercise);
         break;
       case "Ноги":
+        setActiveMuscleGroup(name);
         setActiveExercise(muscleGroup[7].exercise);
         break;
       default:
